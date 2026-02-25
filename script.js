018b09200e78f03ede03422fdb14545aeebe6a40
Illy Hoang <133312266+illydh@users.noreply.github.com>
2026-02-24 18:04:15 -0800

    feat: dark mode toggler

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function updateThemeIcons() {
+  const isDark = document.body.classList.contains("dark-mode");
+  const checkbox = document.getElementById("checkbox");
+
+  if (checkbox) {
+    checkbox.checked = isDark;
+  }
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+  const isDark = document.body.classList.contains("dark-mode");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+  updateThemeIcons();
+}
+
+// Load earlier stored theme
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+  updateThemeIcons();
+});
